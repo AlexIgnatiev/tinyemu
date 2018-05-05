b8eae437cd012bf258f1df6809029dd567055c3c
Alex Ignatiev <alexandr.ignatiev@ist.utl.pt>
2018-05-05 18:49:19 +0100

    profile script with prob. support

--- plot/plot.sh
+++ plot/plot.sh
@@ -5,20 +5,23 @@ git stash
 git pull origin elsa
 make clean
 make
+PROB=50
+GPU_FILENAME=gpu_p$PROB.txt
+CPU_FILENAME=cpu_p$PROB.txt
 
 sizes=(65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216 33554432 67108864 134217728)
 iters=11
-echo -n "" > cpu.txt
-echo -n "" > gpu.txt
+echo -n "" > $CPU_FILENAME
+echo -n "" > $GPU_FILENAME
 for SIZE in ${sizes[@]}
 do
-    echo -n $SIZE"," >> cpu.txt
-    echo -n $SIZE"," >> gpu.txt
+    echo -n $SIZE"," >> CPU_FILENAME
+    echo -n $SIZE"," >> GPU_FILENAME
 	for ((IT=0; IT<$iters; IT++))
     do
-        echo -n $(LD_LIBRARY_PATH=/opt/intel/opencl-sdk/lib64/ ./program 1 $SIZE)";" >> cpu.txt
-        echo -n $(LD_LIBRARY_PATH=/opt/intel/opencl-sdk/lib64/ ./program 0 $SIZE)";" >> gpu.txt
+        echo -n $(LD_LIBRARY_PATH=/opt/intel/opencl-sdk/lib64/ ./program -s $SIZE -d cpu -p $PROB)";" >> CPU_FILENAME
+        echo -n $(LD_LIBRARY_PATH=/opt/intel/opencl-sdk/lib64/ ./program -s $SIZE -d gpu -p $PROB)";" >> GPU_FILENAME
     done
-    echo "" >> cpu.txt
-    echo "" >> gpu.txt
+    echo "" >> CPU_FILENAME
+    echo "" >> GPU_FILENAME
 done
